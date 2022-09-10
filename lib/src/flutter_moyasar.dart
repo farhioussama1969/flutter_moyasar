56f96d412d9c186bd8ad4e4c3079c9ffa0ef3d94
Farhi oussama baha eddine <farhioussama1969@gmail.com>
2022-09-10 21:33:39 +0100

    first commit

---
+++ lib/src/flutter_moyasar.dart
@@ -0,0 +1,9 @@
+class FlutterMoyasar {
+  void creditCardPayment() {
+    print('this is a credit card payment method');
+  }
+
+  void applePayPayment() {
+    print('this is a apple pay payment method');
+  }
+}
